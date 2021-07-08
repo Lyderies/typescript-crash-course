c0e0377e010773c26283f4968062ca50ce3187c7
Lyderies <lyderies@hotmail.com>
2021-07-09 11:51:04 +1200

    object types module complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 // Module Imports
-import UsingTypes from './modules/UsingTypes';
+// import UsingTypes from './modules/UsingTypes';
+// import ObjectTypes from "./modules/ObjectTypes";
+
+
 
 
 
 const App = () => {
   return (
     <div className="App">
-      <UsingTypes />
+      {/* <UsingTypes /> */}
+      {/* <ObjectTypes /> */}
     </div>
   );
 }
---
+++ src/modules/ObjectTypes.tsx
@@ -0,0 +1,23 @@
+// Object Types Module
+
+const ObjectTypes = () => {
+
+    // Object types
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: "Maximilian",
+    age: 30
+}
+
+console.log(person)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ObjectTypes
